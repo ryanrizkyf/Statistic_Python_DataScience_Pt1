0b09fe33fa085c87cbca78817745ac20682b07fe
Ryan <ryanrizkyf@gmail.com>
2021-03-21 16:31:49 +0700

    edit commit

---
+++ Pengenalan_NumpyPandas/Load_Library.py
@@ -0,0 +1,9 @@
+# Biasanya pada awal mula sebelum melakukan pengolahan data,
+# kita memanggil library yang diinginkan untuk digunakan dalam analisa data,
+# dalam hal ini kita akan memuat numpy dan pandas.
+
+# memuat numpy sebagai np
+import numpy as np
+
+# memuat pandas sebagai pd
+import pandas as pd
